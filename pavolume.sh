7ef95afa0700e7b34a20cc67a861f5cd839dec43
Alejandro Roldán <alej.roldan.trabajos@gmail.com>
2021-07-13 00:04:28 +0200

    Add files via upload

--- pavolume.sh
+++ pavolume.sh
@@ -19,6 +19,7 @@
 #	pavolume.sh unmute
 #	pavolume.sh toggle   # mute/unmute
 
+
 # Save argument to variable
 opt=$1
 
@@ -27,23 +28,31 @@ MAX=100
 # Get pulse audio active sink
 sink=$(pacmd list-sinks | grep \* | awk '{print $3}')
 
+
 if [[ $opt == 'mute' ]]; then	
 	pactl set-sink-mute $sink 1
+
 elif [[ $opt = 'unmute' ]]; then
 	pactl set-sink-mute $sink 0
+
 elif [[ $opt == 'toggle' ]]; then
 	pactl set-sink-mute $sink toggle
+
 else
+	if [[ $opt == +* ]]; then
+		# Remove the starting "+" to get only the number
+		opt_num=${opt:1}
+		# Get the current volume
+		# head -n $(( $sink +1 )) was used in raspberry, but here that doesn't work and needs to be just $sink
+		vol=$(pacmd list-sinks | grep '^[[:space:]]volume:' | head -n $sink | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+		# Get what would be the new volume
+		new_vol=$(( vol+opt_num ))
+		# If the new volume is above max%
+		if [ $new_vol -gt $MAX ]; then opt=$MAX; fi
+	fi
+
 	# Change active sink volume
 	pactl set-sink-volume $sink $opt%
-
-	# Check the volume
-	new_vol=$(pacmd list-sinks | grep '^[[:space:]]volume:' | head -n $(( $sink + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
-	# If the volume is above max%
-	if [ $new_vol -gt $MAX ]; then
-		# Set it to the max
-		pactl set-sink-volume $sink $MAX%
-	fi
 
 	# Unmute sink in case it was muted
 	pactl set-sink-mute $sink 0
